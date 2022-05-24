c6ea14a1aa24676ddd984b0ab5b2338e94b1f416
FweeGamerHD <freegamerhd12@gmail.com>
2022-05-24 23:31:22 +0200

    Changed a few dependencies, I'm fucking retarded and I will continue tomorrow

--- src/main/java/Musicbot.java
+++ src/main/java/Musicbot.java
@@ -1,18 +1,18 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.security.auth.login.LoginException;
+public class Musicbot extends ListenerAdapter {
 
-public class Musicbot {
-
-    // Main method
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws Exception {
 
         Secretstuff secretstuff = new Secretstuff();
 
         JDA bot = JDABuilder.createDefault(secretstuff.getToken())
+                .addEventListeners(new Musicbot())
                 .setActivity(Activity.listening("to p!"))
                 .build();
     }
+
 }
